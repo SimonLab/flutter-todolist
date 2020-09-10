685ca88f2c2ef8f2ad34df73ee1c5df00e5b5a79
SimonLab <simon.labondance@gmail.com>
2020-09-10 14:43:32 +0100

    create new item using TextField widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,70 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(title: 'Todo', home: TodoList()));
+  runApp(MaterialApp(title: 'Todo', home: App()));
 }
 
-class TodoList extends StatelessWidget {
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('Todolist')), body: Text('list of items'));
+        appBar: AppBar(
+          title: Text('TodoList'),
+        ),
+        body: TodoList());
+  }
+}
+
+class Item {
+  const Item({this.value});
+  final String value;
+}
+
+class ShowItem extends StatelessWidget {
+  ShowItem({this.item});
+
+  final Item item;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(item.value),
+    );
+  }
+}
+
+class TodoList extends StatefulWidget {
+  @override
+  _TodoListState createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  List<Item> _items = [];
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Expanded(
+          child: ListView(
+        children: _items.map((Item item) {
+          return ShowItem(item: item);
+        }).toList(),
+      )),
+      TextField(
+        controller: _controller,
+        decoration: InputDecoration(
+            border:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.teal)),
+            labelText: 'new item'),
+        onSubmitted: (newItem) {
+
+          setState(() {
+            _items.add(Item(value: newItem));
+            _controller.clear();
+          });
+        },
+      )
+    ]);
   }
 }
